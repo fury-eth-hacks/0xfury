92764e599c18a4b7b4cb63bc6d8fc528b867d29a
waynedmustt <muhammaddimas04@gmail.com>
2023-09-11 21:51:29 +0800

    PWA - frontend2

--- frontend2/src/main.tsx
+++ frontend2/src/main.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-// import { SmartAccountProvider  } from './contexts/SmartAccountAcontext'
-// import { Web3AuthProvider } from './contexts/SocialLoginContext'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <Web3AuthProvider>
-      <SmartAccountProvider> */}
         <App />
-      {/* </SmartAccountProvider>
-    </Web3AuthProvider> */}
   </React.StrictMode>,
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
